edb6e96dfce0af639018d86cfa9cc56ed7c86cb2
Peter Phan <62064025+phanthang1994@users.noreply.github.com>
2024-08-11 00:19:49 +0700

    Header UI 3

--- src/componenents/Layout/components/Header/index.js
+++ src/componenents/Layout/components/Header/index.js
@@ -3,25 +3,29 @@ import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
 import images from "~/assets/images";
 import Tippy from "@tippyjs/react/headless";
-import { Wrapper as PropperWreapper }  from '~/componenents/Propper';
+import { Wrapper as PropperWreapper } from "~/componenents/Propper";
 // import Tippy from '@tippyjs/react';
 // import 'tippy.js/dist/tippy.css'; // optional
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCircleXmark,
   faMagnifyingGlass,
+  faSignIn,
   faSpinner,
 } from "@fortawesome/free-solid-svg-icons";
 import AccountItem from "~/componenents/Accountitem";
+import Button from "~/componenents/Button";
 const cx = classNames.bind(styles);
 function Header() {
   const [searchResult, setSearchResult] = useState([]);
   setTimeout(() => {
     setSearchResult(() => {
-      return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-    })
-  },0)
- 
+      return [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      ];
+    });
+  }, 0);
+
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
@@ -29,19 +33,17 @@ function Header() {
           <img src={images.logo} alt="Tiktok" />
         </div>
         <Tippy
-        interactive={true}
-        visible={searchResult.length > 0}
+          interactive={true}
+          visible={searchResult.length > 0}
           render={(attrs) => (
-              <div className={cx("search-result")} tabIndex="-1" {...attrs}>
-            <PropperWreapper>
-                <h4 className={cx("search-title")}>
-                    Accounts
-                </h4>
+            <div className={cx("search-result")} tabIndex="-1" {...attrs}>
+              <PropperWreapper>
+                <h4 className={cx("search-title")}>Accounts</h4>
                 <AccountItem></AccountItem>
                 <AccountItem></AccountItem>
                 <AccountItem></AccountItem>
-            </PropperWreapper>
-              </div>
+              </PropperWreapper>
+            </div>
           )}
         >
           <div className={cx("search")}>
@@ -62,7 +64,11 @@ function Header() {
           </div>
         </Tippy>
 
-        <div className={cx("actions")}></div>
+        <div className={cx("actions")}>
+          <Button text>Upload</Button>
+          <Button primary leftIcon ={<FontAwesomeIcon icon={faSignIn} />}>Login</Button>
+          {/* <Button   rouned>Get App</Button> */}
+        </div>
       </div>
     </header>
   );
